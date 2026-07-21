e2779e4cf8b773773804e7bdb9bb89a7c508fc9c
sfmuhit <sfmuhit25@gmail.com>
2026-07-21 10:07:41 +0600

    3

--- Chai aur code DJango/chaiaurDjango/chaiaurDjango/views.py
+++ Chai aur code DJango/chaiaurDjango/chaiaurDjango/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render 
 
 def home(request):
-    return HttpResponse("Hello,world. You are at chai aur Django Home page")
+    # return HttpResponse("Hello,world. You are at chai aur Django Home page")
+    return render(request,'website/index.html')
+
+# def about(request):
+#     return HttpResponse("Hello,world. You are at chai aur Django About page")
+
+# def contact(request):
+#     return HttpResponse("Hello,world. You are at chai aur Django contact page")
 
 def about(request):
-    return HttpResponse("Hello,world. You are at chai aur Django About page")
+    return render(request,'website/about.html')
 
 def contact(request):
-    return HttpResponse("Hello,world. You are at chai aur Django contact page")
+    return render(request,'website/contact.html')
